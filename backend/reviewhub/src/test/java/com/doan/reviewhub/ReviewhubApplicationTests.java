96db68d4bc355f62829204a22d70ef436316fa69
sniperdarknesspro <njstpham377@gmail.com>
2026-04-14 05:17:59 +0700

    Initial commit: Add Java Spring Boot backend for ReviewHub

---
+++ backend/reviewhub/src/test/java/com/doan/reviewhub/ReviewhubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doan.reviewhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
